955a9019748fb5c089e82a92900f581bcd37ff10
Kyanka <mozgoprod@gmail.com>
2021-06-24 20:42:36 +0300

    Radency test task ver 0.3

--- TestTask/TestTask.js
+++ TestTask/TestTask.js
@@ -5,28 +5,40 @@ const chooseBestDistance = (t, k, ls) => {
     }         //function to check if items are positive
     const isInt = (n) => {
         return (n ^ 0) === n;
-    }       //function to check if items are integer
+    }              //function to check if items are integer
+    const s = (a, b) => a + b;              //function of sum
     if (k <= ls.length && k >= 1 && ls.length >= 1 && ls.every(isPositive) && ls.every(isInt)) { // initial check of input
-        let arr = ls;                   //replication of ls to avoid changes of initial array
-        arr.sort();                     //sort array to minimise iterations
-        const s = (a, b) => a + b;      //function of sum
-        let cities = [];                //array for saving best distances
-        for (let j = 0; j <= arr.length - 1; j++) {                 //cycle for finding best distance triple set
-            let c = []                                              //array for saving current distances
-            let i = j
-            for (i; c.length <= k && i <= arr.length - 1; i++) {    //cycle for finding distance sets
-                let sumMiles = c.reduce(s, 0);                      //current sum
-                let item = arr[arr.length - 1 - i];                 //current item of array
-                if (sumMiles < t && sumMiles + item <= t) {
-                    c.push(item);                                   //creation of set
+        const c = () => {
+            let res = null;
+            const combinations = (arr, k, start, idx, current) => {
+                if (idx === k) {
+                    res.push([...current]);
+                    return;
+                }
+                for (let i = start; i < arr.length; i += 1) {
+                    current[idx] = arr[i];
+                    combinations(arr, k, i + 1, idx + 1, current);
                 }
             }
-            if (c.reduce(s, 0) >= cities.reduce(s, 0) && c.reduce(s, 0) <= t && c.length === k) {
-                cities = c;                                         //creation of best triple set
-            }
+            return (arr, k) => {
+                res = [];
+                combinations(arr, k, 0, 0, []);
+                const temp = res;
+                res = null;
+                return temp;
+            };
         }
-        if (cities.length === k)
-            return cities.reduce(s, 0)                              // return best distance
+        const combine = (c());
+        //
+        let combinations = combine(ls, k)   //create combinations
+        let bestSum = 0;
+        combinations.forEach(c => {         //choose best sum
+            let sum = c.reduce(s, 0);
+            if(sum >= bestSum && sum <= t ){
+                bestSum = sum;
+            }
+        } );
+        return bestSum
     }
     //myCodeEnd
     return null;
@@ -34,5 +46,3 @@ const chooseBestDistance = (t, k, ls) => {
 
 chooseBestDistance(174, 3, [51, 56, 58, 59, 61]); //173
 chooseBestDistance(163, 3, [50]); // null
-
-
